11cee250f073241b431b345909f4a65d522f95cb
Admir Trakic <atrakic@users.noreply.github.com>
2022-11-04 19:34:45 +0100

    Initial

---
+++ src/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(
+		w, `
+Hello from azure-webapp-deploy!
+`,
+	)
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", handler)
+	fmt.Println("Go backend started!")
+	log.Fatal(http.ListenAndServe(":80", r))
+}
